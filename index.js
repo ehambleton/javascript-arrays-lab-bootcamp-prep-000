a68602694ac2899ed7c673cb753e255de3e0f65b
Edward Hambleton <ehambleton@me.com>
2017-12-14 02:50:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ kittens.push(name)
 return kittens
 }
 
+function destructivelyPrependKitten(name) {
+kittens.unshift(name)
+return kittens
+}
+
 function destructivelyRemoveLastKitten() {
 kittens.pop()
 return kittens
